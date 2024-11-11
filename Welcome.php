f6354a04e5137130e9d1d82303be5a9b6e01c324
Gouse.C <140190450+Gouse-c@users.noreply.github.com>
2024-11-11 23:26:42 +0530

    Add files via upload

---
+++ Welcome.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Welcome to Aditya University</title>
+</head>
+<body>
+<h2>Hellooooooooo Adityanssssssssssssssss</h2>
+<?php
+echo "Welcome to the Colours Event-2025";
+?>
+</body>
+</html>
